6956f2af7536c5f8d1a0fd7711776e02a03b7bee
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2024-03-13 14:35:05 -0500

    add routes

---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,6 @@
+<h2>About Me</h2>
+
+<a href="/">Home</a>
+
+<div class="h-96"></div>
+<div class="h-96"></div>
---
+++ src/routes/theatre/+page.svelte
@@ -0,0 +1,6 @@
+<h2>Theatre</h2>
+
+<a href="/">Home</a>
+
+<div class="h-96"></div>
+<div class="h-96"></div>
