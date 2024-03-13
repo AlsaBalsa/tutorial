ac4d85fc1d4c851400132054fac5c26ea6b23689
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2024-03-13 14:33:58 -0500

    add data/config

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,54 @@
 // place files you want to import through the `$lib` alias in this folder.
+export interface Link {
+  href: string;
+  title: string;
+}
+
+export interface LinkSection {
+  title: string;
+  links: Link[];
+}
+
+export const pages: LinkSection[] = [
+  {
+    title: "Main",
+    links: [
+      {
+        title: "About Me",
+        href: "/about-me",
+      },
+      {
+        title: "Theatre",
+        href: "/theatre",
+      },
+    ],
+  },
+  {
+    title: "Memes",
+    links: [
+      {
+        title: "About Me",
+        href: "/about-me",
+      },
+      {
+        title: "Theatre",
+        href: "/theatre",
+      },
+    ],
+  },
+  {
+    title: "Memes",
+    links: [
+      {
+        title: "About Me",
+        href: "/about-me",
+      },
+      {
+        title: "Theatre",
+        href: "/theatre",
+      },
+    ],
+  },
+];
+
+export const title = "Allistar Barrett";
