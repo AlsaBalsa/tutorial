65c9e018dbb6861922978f347e7bf133497a6fbc
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2024-03-13 14:34:40 -0500

    add main layout

---
+++ src/lib/layouts/MainLayout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+
+	import type { LinkSection } from '$lib';
+
+	export let pages: LinkSection[];
+	export let title: string;
+</script>
+
+<Header {pages} {title} />
+
+<slot />
+
+<Footer {pages} {title} />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+
+	import { pages, title } from '$lib';
+
+	import MainLayout from '$lib/layouts/MainLayout.svelte';
 </script>
 
-<slot />
+<MainLayout {pages} {title}>
+	<slot />
+</MainLayout>
