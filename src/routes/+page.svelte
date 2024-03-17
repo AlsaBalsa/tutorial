1a488915b162ccd461d674d41f94ad9ae5584503
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2024-03-17 13:54:48 -0500

    create example images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { pages } from '$lib';
+</script>
+
+<div class="grid md:grid-cols-2 grid-cols-1 grid-flow-row justify-items-center w-1/2 mx-auto p-3">
+	{#each pages as { title }}
+		<div class="p-2">
+			<div class="h-56 w-36 bg-gradient-to-tr from-red-300 to-green-300 p-2"></div>
+			<p class="w-full text-center p-2">{title}</p>
+		</div>
+	{/each}
+</div>
 
 <div class="h-96"></div>
 <div class="h-96"></div>
